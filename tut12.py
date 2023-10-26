65b8c735dbcdf22bf809341bb9147fbf3f525ae9
RAJJIT <2.0lairaj@gmail.com>
2023-10-26 23:14:53 +0530

    tut12

---
+++ tut12.py
@@ -0,0 +1,20 @@
+# Python Modules - uses
+
+'''Definition: A python file that contains some function definitions and statements '''
+# from tut11 import factorial
+# output = factorial(5)
+# print("the factorial is", output)
+
+# import random
+
+# random_num = random.randint(10, 50)
+# print(random_num)
+
+# import calendar
+# print(calendar.month(2025, 12))
+
+from turtle import*
+while True:
+    forward(100)
+    left(100)
+
