fb52242109206b04e307ce048651a36fa51505f1
RAJJIT <2.0lairaj@gmail.com>
2023-10-30 23:03:40 +0530

    tut13-14

---
+++ tut13.py
@@ -0,0 +1,13 @@
+# FILE IO BASICS
+
+# two types: 1 binary and 2 text
+
+r - open for reading - by default
+w - open for writing
+x - creates a file if does not exist
+a - appends or adds contents to a file
+
+t - for opening in text format
+b - for opening in binary format
+
++ - opening for both read and write
---
+++ tut14.py
@@ -0,0 +1,11 @@
+# open and closing functions with commands
+
+# fileCon = open("file.txt", "rb") # in read mode - by default
+#fileCon = open("file.txt", "rb") # in binary - by default
+
+#print(fileCon.read())  # using read function
+
+f = open("file.txt", "rt")
+content = f.read()
+print(content)
+f.close()
