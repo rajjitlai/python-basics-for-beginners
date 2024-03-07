c296a6562ae75b3a3f2ce07dcd1af361efcbf88d
RAJJIT <2.0lairaj@gmail.com>
2024-03-07 23:18:15 +0530

    tut-21-ii

---
+++ tut21-ii.py
@@ -0,0 +1,54 @@
+# methods of Dictionary
+
+new_dict = {"Roll_no": "B120", "Name": "Jamesbond", "Course": "BTech"}
+print("The dictionary is:",new_dict)
+
+# len
+# print(len(new_dict))
+
+# str
+# print(str(new_dict))
+
+# clear
+# new_dict.clear()
+# print("After clearing,", new_dict)
+
+# copy
+# new_dict2 = new_dict.copy()
+# print(new_dict2)
+
+# get(key)
+# dataFetch = new_dict.get("Roll_no")
+# print(dataFetch)
+
+# has key - in and not in
+# checkData = "Roll no" in new_dict  # if yes - TRUE, no - FALSE
+# print(checkData)
+
+# items - returns a tuple
+# itemDict = new_dict.items()
+# print(itemDict)
+
+# keys - returns all the keys in the dictionary
+# getKeys = new_dict.keys()
+# print(getKeys)
+
+# setdefault
+# new_dict.setdefault('Marks', 0)
+# new_dict.setdefault('Phone number', 'NA')
+# print(new_dict)
+
+# update 
+# new_dict2 = {"Marks": 94, "Age": 21}
+# new_dict.update(new_dict2)
+
+# print(new_dict)
+
+# values - returns only the values in the dictionary
+getValues = new_dict.values()
+print(getValues)
+
+# Program
+# Create your own dictionary using two Dictionary in python. Users can add or delete or make changes to the dictionary.
+
+# comment your code below - Comment box ta code happirmmo
