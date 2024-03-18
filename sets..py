55b2c389dad941d43295d8b9a09a045e715487c8
RAJJIT <2.0lairaj@gmail.com>
2024-03-18 22:52:19 +0530

    tut-22

---
+++ sets..py
@@ -0,0 +1,88 @@
+# update
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+set1.update(set2)
+print(set1)
+
+# add
+set1.add(6)
+print(set1)
+
+# remove
+set1.remove(3)
+print(set1)
+
+# discard
+set1.discard(7)
+
+# pop
+popped_element = set1.pop()
+print(popped_element)
+
+# clear
+set1.clear()
+print(set1)
+
+# length
+length = len(set1)
+print(length)
+
+# in and not in
+print(2 in set1)
+print(3 not in set1)
+
+# issubset or s≤ t
+set2 = {4, 5}
+print(set2.issubset(set1))
+
+# issuperset or s≥ t
+print(set1.issuperset(set2))
+
+# union(t) or s|t
+union_set = set1.union(set2)
+
+# intersection(t) or s&t
+intersection_set = set1.intersection(set2)
+
+# intersection_update(t)
+set1.intersection_update(set2)
+
+# difference(t) or s-t
+difference_set = set1.difference(set2)
+
+# difference_update(t)
+set1.difference_update(set2)
+
+# symmetric_difference(t)  or s^t
+symmetric_difference_set = set1.symmetric_difference(set2)
+
+# copy()
+copied_set = set1.copy()
+
+# isdisjoint(t)
+disjoint = set1.isdisjoint(set2)
+
+# all(s)
+all_true = all(set1)
+for x in set1:
+    print(x)
+
+# enumerate(s)
+enum_set = enumerate(set1)
+
+# max(s)
+max_element = max(set1)
+
+# min(s)
+min_element = min(set1)
+
+# sum(s)
+sum_of_elements = sum(set1)
+
+# sorted(s)
+sorted_list = sorted(set1)
+
+# s==t and s≠t
+equal = set1 == set2
+not_equal = set1 != set2
+
---
+++ tut22.py
@@ -0,0 +1,13 @@
+# sets
+
+# creation of sets - basic syntax
+# setCreate = {90, 77, 10, 1, 56}
+# print(setCreate)
+# print(type(setCreate))
+
+set1 = {1, 2, 3, 33, 10, 23, 0, 55, 120, 99, 12}
+set2 = {4, 5, 6, 7}
+equal = set1 == set2
+not_equal = set1 != set2
+print(equal)
+print(not_equal)
