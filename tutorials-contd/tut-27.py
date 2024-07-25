420ef927ba550626d828fecb0cbf8468a0e2495b
RAJJIT <2.0lairaj@gmail.com>
2024-07-25 22:01:03 +0530

    tut-27-added

---
+++ tutorials-contd/tut-27.py
@@ -0,0 +1,26 @@
+# Inheritance
+# We have already discussed about it
+
+# Abstraction
+"""
+Abstraction is a concept in programming that focuses on hiding the complex implementation details and showing only the essential features of an object. It allows programmers to focus on what an object does rather than how it does it. In Python, abstraction is achieved using abstract classes and interfaces, allowing the creation of a blueprint for other classes.
+> In short, it means to keep hidden.
+"""
+
+# Encapsulation
+"""
+Encapsulation is the practice of bundling data and methods that operate on that data within a single unit, typically a class. It restricts direct access to some of an object's components, which is a means of preventing unintended interference and misuse. In Python, encapsulation is achieved using private and protected access modifiers.
+> In short, the elements are kept inside a capsule (guli in Manipuri)
+"""
+
+# Reusability
+"""
+Reusability in programming refers to the ability to use components of code in different programs or in different parts of the same program without modification. It promotes efficient code management and reduces redundancy. In Python, reusability is often achieved through inheritance, modules, and libraries, enabling developers to build on existing work.
+> In short, elements can be used again and again
+"""
+
+# Polymorphism
+"""
+Polymorphism allows objects of different classes to be treated as objects of a common superclass. It enables a single interface to represent different underlying data types. In Python, polymorphism is achieved through method overriding and interfaces, allowing functions to use objects of different types at different times, making code more flexible and reusable.
+> In short, it is the ability to take more than one form.
+"""
